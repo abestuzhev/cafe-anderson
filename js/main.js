6b5ea5e9ea7b9afba726f13ef37b4af343cad9e2
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-10-23 01:32:52 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -15,11 +15,21 @@ $(document).ready(function ($) {
     function showPopup(icon, popup) {
         $(icon).on('click', function (e) {
             e.preventDefault();
-            $(popup).addClass('popup-show');
-            $(popup).parents('.fixed-overlay').addClass('is-visible');
-            $('body').addClass('body-popup');
+            $(popup).addClass('visible-test');
+            $('.mfp-bg').addClass('visible-test');
+            $('html').css('overflow','hidden');
+            // $('html').addClass('body-popup');
         });
     }
+
+
+    $(".popup-close").click(function (e) {
+        e.preventDefault();
+        $(this).parents('.mfp-wrap').removeClass('visible-test');
+        $('.mfp-bg').removeClass('visible-test');
+        $('html').css('overflow','auto');
+        // $('html').removeClass('body-popup');
+    });
 
     showPopup(".header-phone", '.popup__request-call');
     showPopup(".header-email", '.popup__write-to-us');
@@ -44,18 +54,12 @@ $(document).ready(function ($) {
         $('.popup-authorization').parents('.fixed-overlay').addClass('is-visible');
     });
 
-    $(".popup-close").click(function (e) {
-        e.preventDefault();
-        $(this).parents('.popup').removeClass('popup-show');
-        $('.fixed-overlay').removeClass('is-visible');
-        $('body').removeClass('body-popup');
-    });
 
 
     $("#mobile-reg").click(function (e) {
         e.preventDefault();
-        $('.popup').removeClass('popup-show');
-        $('.popup-reg').addClass('popup-show');
+        $('.popup').removeClass('visible-test');
+        $('.popup-reg').addClass('visible-test');
         $('.popup-reg').parents('.fixed-overlay').addClass('is-visible');
         $('body').addClass('body-popup');
     });
@@ -107,12 +111,11 @@ $(document).ready(function ($) {
     $(".popup-bg").click(function (e) {
         e.preventDefault();
         $('.popup').removeClass('popup-show');
-        $(this).removeClass('is-visible');
+        $('.fixed-overlay').removeClass('is-visible');
         $('body').removeClass('body-popup');
     });
 
     /*клик вне области экрана для корзины*/
-
     function hidePopup (element, instrumentHide){
         $(document).mouseup(function (e) { // событие клика по веб-документу
             var div = $(element); // тут указываем ID элемента
@@ -128,16 +131,16 @@ $(document).ready(function ($) {
     hidePopup('.popup-basket', 'popup-slide__show');
     // hidePopup('.popup', 'popup-show');
     hidePopup('.header-mobile__auth', 'popup-slide__show');
-
-    $(document).mouseup(function (e) { // событие клика по веб-документу
-        var div = $('.popup-body'); // тут указываем ID элемента
-        if (!div.is(e.target) // если клик был не по нашему блоку
-            && div.has(e.target).length === 0) { // и не по его дочерним элементам
-            div.parents('.popup').removeClass('popup-show'); // скрываем его
-            $('.fixed-overlay').removeClass('is-visible');
-            $('body').removeClass('body-popup');
-        }
-    });
+    //
+    // $(document).mouseup(function (e) { // событие клика по веб-документу
+    //     var div = $('.popup-body'); // тут указываем ID элемента
+    //     if (!div.is(e.target) // если клик был не по нашему блоку
+    //         && div.has(e.target).length === 0) { // и не по его дочерним элементам
+    //         div.parents('.popup').removeClass('popup-show'); // скрываем его
+    //         $('.fixed-overlay').removeClass('is-visible');
+    //         $('body').removeClass('body-popup');
+    //     }
+    // });
 
     // мобильный заказать звонок, показываем форму и удаляем надпись
     $(".mobile-request-call_inquiry").click(function (e) {
@@ -282,6 +285,8 @@ $(document).ready(function ($) {
         }
     });
 
+});
+
 
 
     /*-------------------------------------------------------------*/
@@ -291,115 +296,114 @@ $(document).ready(function ($) {
 // });
 
     //------------------------------------------------------------custom###
+//
+// 	$body = $('body');
+// 	windowWidth = $(window).width();
+// 	windowHeight = $(window).height();
+//
+// 	pageWidget(['index']);
+// 	getAllClasses('html','.elements_list');
+// });
+//
+// $(window).on('load', function () {
+// 	loadFunc();
+// });
+//
+// $(window).on('resize', function () {
+// 	resizeFunc();
+// });
+//
+// $(window).on('scroll', function () {
+// 	scrollFunc();
+// });
+//
+// function loadFunc() {
+//
+// }
+// function resizeFunc() {
+// 	updateSizes();
+// }
+//
+// function scrollFunc() {
+// 	updateSizes();
+// }
+//
+// function updateSizes() {
+// 	windowWidth = $(window).width();
+// 	windowHeight = $(window).height();
+// }
 
-	$body = $('body');
-	windowWidth = $(window).width();
-	windowHeight = $(window).height();
-
-	//developer funcitons
-	pageWidget(['index']);
-	getAllClasses('html','.elements_list');
-});
-
-$(window).on('load', function () {
-	loadFunc();
-});
-
-$(window).on('resize', function () {
-	resizeFunc();
-});
-
-$(window).on('scroll', function () {
-	scrollFunc();
-});
-
-function loadFunc() {
-
-}
-function resizeFunc() {
-	updateSizes();
-}
-
-function scrollFunc() {
-	updateSizes();
-}
-
-function updateSizes() {
-	windowWidth = $(window).width();
-	windowHeight = $(window).height();
-}
-
-if ('objectFit' in document.documentElement.style === false) {
-	document.addEventListener('DOMContentLoaded', function () {
-		Array.prototype.forEach.call(document.querySelectorAll('img[data-object-fit]'), function (image) {
-			(image.runtimeStyle || image.style).background = 'url("' + image.src + '") no-repeat 50%/' + (image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'));
-
-			image.src = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'' + image.width + '\' height=\'' + image.height + '\'%3E%3C/svg%3E';
-		});
-	});
-}
+// if ('objectFit' in document.documentElement.style === false) {
+// 	document.addEventListener('DOMContentLoaded', function () {
+// 		Array.prototype.forEach.call(document.querySelectorAll('img[data-object-fit]'), function (image) {
+// 			(image.runtimeStyle || image.style).background = 'url("' + image.src + '") no-repeat 50%/' + (image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'));
+//
+// 			image.src = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'' + image.width + '\' height=\'' + image.height + '\'%3E%3C/svg%3E';
+// 		});
+// 	});
+// }
 
 //Functions for development
-function getAllClasses(context, output) {
-	var finalArray = [],
-		mainArray = [],
-		allElements = $(context).find($('*'));//get all elements of our page
-	//If element has class push this class to mainArray
-	for (var i = 0; i < allElements.length; i++) {
-		var someElement = allElements[i],
-			elementClass = someElement.className;
-		if (elementClass.length > 0) {//if element have not empty class
-			//If element have multiple classes - separate them
-			var elementClassArray = elementClass.split(' '),
-				classesAmount = elementClassArray.length;
-			if (classesAmount === 1) {
-				mainArray.push('.' + elementClassArray[0] + ' {');
-			} else {
-				var cascad = '.' + elementClassArray[0] + ' {';
-				for (var j = 1; j < elementClassArray.length; j++) {
-					cascad += ' &.' + elementClassArray[j] + ' { }';
-				}
-				mainArray.push(cascad);
-			}
-		}
-	}
+// function getAllClasses(context, output) {
+// 	var finalArray = [],
+// 		mainArray = [],
+// 		allElements = $(context).find($('*'));//get all elements of our page
+// 	//If element has class push this class to mainArray
+// 	for (var i = 0; i < allElements.length; i++) {
+// 		var someElement = allElements[i],
+// 			elementClass = someElement.className;
+// 		if (elementClass.length > 0) {//if element have not empty class
+// 			//If element have multiple classes - separate them
+// 			var elementClassArray = elementClass.split(' '),
+// 				classesAmount = elementClassArray.length;
+// 			if (classesAmount === 1) {
+// 				mainArray.push('.' + elementClassArray[0] + ' {');
+// 			} else {
+// 				var cascad = '.' + elementClassArray[0] + ' {';
+// 				for (var j = 1; j < elementClassArray.length; j++) {
+// 					cascad += ' &.' + elementClassArray[j] + ' { }';
+// 				}
+// 				mainArray.push(cascad);
+// 			}
+// 		}
+// 	}
 
 	//creating finalArray, that don't have repeating elements
-	var noRepeatingArray = unique(mainArray);
-	noRepeatingArray.forEach(function (item) {
-		var has = false;
-		var preWords = item.split('&');
-		for (var i = 0; i < finalArray.length; ++i) {
-			var newWords = finalArray[i].split('&');
-			if (newWords[0] == preWords[0]) {
-				has = true;
-				for (var j = 0; j < preWords.length; ++j) {
-					if (newWords.indexOf(preWords[j]) < 0) {
-						newWords.push(preWords[j]);
-					}
-				}
-				finalArray[i] = newWords.join('&');
-			}
-		}
-		if (!has) {
-			finalArray.push(item);
-		}
-	});
-	for (var i = 0; i < finalArray.length; i++) {
-		$('<div>' + finalArray[i] + ' }</div>').appendTo(output);
-	}
-
-	//function that delete repeating elements from arrays, for more information visit http://mathhelpplanet.com/static.php?p=javascript-algoritmy-obrabotki-massivov
-	function unique(A) {
-		var n = A.length, k = 0, B = [];
-		for (var i = 0; i < n; i++) {
-			var j = 0;
-			while (j < k && B[j] !== A[i]) j++;
-			if (j == k) B[k++] = A[i];
-		}
-		return B;
-	}
-}
+// 	var noRepeatingArray = unique(mainArray);
+// 	noRepeatingArray.forEach(function (item) {
+// 		var has = false;
+// 		var preWords = item.split('&');
+// 		for (var i = 0; i < finalArray.length; ++i) {
+// 			var newWords = finalArray[i].split('&');
+// 			if (newWords[0] == preWords[0]) {
+// 				has = true;
+// 				for (var j = 0; j < preWords.length; ++j) {
+// 					if (newWords.indexOf(preWords[j]) < 0) {
+// 						newWords.push(preWords[j]);
+// 					}
+// 				}
+// 				finalArray[i] = newWords.join('&');
+// 			}
+// 		}
+// 		if (!has) {
+// 			finalArray.push(item);
+// 		}
+// 	});
+// 	for (var i = 0; i < finalArray.length; i++) {
+// 		$('<div>' + finalArray[i] + ' }</div>').appendTo(output);
+// 	}
+//
+// 	//function that delete repeating elements from arrays, for more information visit http://mathhelpplanet.com/static.php?p=javascript-algoritmy-obrabotki-massivov
+// 	function unique(A) {
+// 		var n = A.length, k = 0, B = [];
+// 		for (var i = 0; i < n; i++) {
+// 			var j = 0;
+// 			while (j < k && B[j] !== A[i]) j++;
+// 			if (j == k) B[k++] = A[i];
+// 		}
+// 		return B;
+// 	}
+// }
 /*
 function pageWidget(pages) {
 	var widgetWrap = $('<div class="widget_wrap"><ul class="widget_list"></ul></div>');
