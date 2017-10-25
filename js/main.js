0e61805ed26d463f58ee49a0a6a1a94d0feac756
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-10-26 00:55:57 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -312,6 +312,19 @@ $(document).ready(function ($) {
         }
     });
 
+    /*кастомный селект*/
+    $('.c-select').SumoSelect();
+
+    /*простые табы*/
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
 });
 
 
