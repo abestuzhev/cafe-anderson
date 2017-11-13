80a5074786d7285ee70409dfecc07a083fa93077
Aleksey <aleksey.bestuzhev@gmail.com>
2017-11-13 14:57:38 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -13,13 +13,43 @@ var $body,
 
 
 //загрузка скрипта
-var scriptMap = document.createElement('script');
-scriptMap.src = "https://api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects&lang=ru-RU";
-document.documentElement.appendChild(scriptMap);
+// var scriptMap = document.createElement('script');
+// scriptMap.src = "https://api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects&lang=ru-RU";
+// document.documentElement.appendChild(scriptMap);
 
-var scriptTool = document.createElement('script');
-scriptTool.src = "https://abestuzhev.github.io/cafe-anderson/bower_components/tooltipster/dist/js/tooltipster.bundle.js";
-document.documentElement.appendChild(scriptTool);
+// var scriptTool = document.createElement('script');
+// scriptTool.src = "https://abestuzhev.github.io/cafe-anderson/bower_components/tooltipster/dist/js/tooltipster.bundle.js";
+// document.documentElement.appendChild(scriptTool);
+
+
+// функция подключения всех скриптов
+
+function dynamicallyLoadScript() {
+
+    var script,
+        box = document.querySelector('.scripts'),
+        src = [
+            'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
+            'https://abestuzhev.github.io/cafe-anderson/js/custom-map.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/preloader.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/jquery-mask-plugin/dist/jquery.mask.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/jscrollpane/jquery.mousewheel.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/jscrollpane/jquery.jscrollpane.min.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/tooltipster/dist/js/tooltipster.bundle.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/simplebar.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/owlcarousel/owl.carousel.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/sumoselect/jquery.sumoselect.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/fotorama/fotorama.js'
+        ];
+
+    for(var i = 0; i < src.length; i++){
+        script = document.createElement("script");
+        script.src = src[i];
+        box.appendChild(script);
+    }  
+};
+
+dynamicallyLoadScript();
 
 
 $(document).ready(function ($) {
@@ -29,7 +59,7 @@ $(document).ready(function ($) {
     // $('#scrollbarY').tinyscrollbar();
 
     //подключение тултипа
-    $ ( '.tooltip' ).tooltipster({
+    $( '.tooltip' ).tooltipster({
         animation: 'grow',
         delay: 100,
         trigger : 'custom' ,
