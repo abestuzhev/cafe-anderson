e1b7c33b03cd5b87dc93fcf7ef0dcc87f04d3414
Aleksey <aleksey.bestuzhev@gmail.com>
2017-11-13 15:28:31 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -22,36 +22,6 @@ var $body,
 // document.documentElement.appendChild(scriptTool);
 
 
-// функция подключения всех скриптов
-
-function dynamicallyLoadScript() {
-
-    var script,
-        box = document.querySelector('.scripts'),
-        src = [
-            'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
-            'https://abestuzhev.github.io/cafe-anderson/js/custom-map.js',
-            'https://abestuzhev.github.io/cafe-anderson/js/preloader.js',
-            'https://abestuzhev.github.io/cafe-anderson/bower_components/jquery-mask-plugin/dist/jquery.mask.js',
-            'https://abestuzhev.github.io/cafe-anderson/js/jscrollpane/jquery.mousewheel.js',
-            'https://abestuzhev.github.io/cafe-anderson/js/jscrollpane/jquery.jscrollpane.min.js',
-            'https://abestuzhev.github.io/cafe-anderson/bower_components/tooltipster/dist/js/tooltipster.bundle.js',
-            'https://abestuzhev.github.io/cafe-anderson/js/simplebar.js',
-            'https://abestuzhev.github.io/cafe-anderson/js/owlcarousel/owl.carousel.js',
-            'https://abestuzhev.github.io/cafe-anderson/bower_components/sumoselect/jquery.sumoselect.js',
-            'https://abestuzhev.github.io/cafe-anderson/bower_components/fotorama/fotorama.js'
-        ];
-
-    for(var i = 0; i < src.length; i++){
-        script = document.createElement("script");
-        script.src = src[i];
-        box.appendChild(script);
-    }  
-};
-
-dynamicallyLoadScript();
-
-
 $(document).ready(function ($) {
     //------------------------------------------------------------custom
 /*scroll*/
--- js/preloader.js
+++ js/preloader.js
@@ -6,3 +6,34 @@ document.body.onload = function(){
         }
     }, 1000);
 };
+
+// функция подключения всех скриптов
+function dynamicallyLoadScript() {
+
+    var script,
+        // box = document.querySelector('.scripts'),
+        src = [
+            // 'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
+            'https://abestuzhev.github.io/cafe-anderson/js/custom-map.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/jquery-mask-plugin/dist/jquery.mask.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/jscrollpane/jquery.mousewheel.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/jscrollpane/jquery.jscrollpane.min.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/tooltipster/dist/js/tooltipster.bundle.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/simplebar.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/owlcarousel/owl.carousel.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/sumoselect/jquery.sumoselect.js',
+            'https://abestuzhev.github.io/cafe-anderson/bower_components/fotorama/fotorama.js',
+            'https://abestuzhev.github.io/cafe-anderson/js/main.js'
+        ];
+
+    for(var i = 0; i < src.length; i++){
+        script = document.createElement("script");
+        script.src = src[i];
+        document.body.appendChild(script);
+    }  
+};
+
+// document.body.onload = function() {
+// };
+
+  dynamicallyLoadScript();
