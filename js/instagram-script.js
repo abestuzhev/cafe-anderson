3f79263181b964db2e5a850d7474bb2ac2942612
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-11-27 11:19:41 +0300

    changes

--- js/instagram-script.js
+++ js/instagram-script.js
@@ -7,16 +7,21 @@ $(document).ready(function() {
     * */
 
     var userFeed = new Instafeed({
+        // get: 'tagged',
         get: 'user',
         userId: '309982629',
         limit: 20,
         resolution: 'standard_resolution',
-        tagName: 'awesome',
+        // tagName: 'людиАндерСона',
         accessToken: '309982629.b57e75e.9741571cf6c840a18a8ada2ae207ef5e',
         sortBy: 'most-recent',
+        // filter: function(image) {
+        //     return image.tags.indexOf('tag') >= 0;
+        // },
         template: '<div class="ins-photo">' +
-        '<a class="ins-photo__img" href="{{link}}" title="{{caption}}" target="_blank">' +
-        '<img src="{{image}}" alt="{{caption}}" class="img-fluid"/>' +
+        '<div class="ins-photo__hash"><a href="{{link}}">{{caption}}</a></div>' +
+        '<a class="ins-photo__img" href="{{link}}" title="" target="_blank">' +
+        '<img src="{{image}}" alt="" class="img-fluid"/>' +
         '</a>' +
         '</div>'
     });
