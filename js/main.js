83cad18968acfc2781c16bc0a3722dfa2436436e
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-03-01 16:20:03 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -145,20 +145,7 @@ $(document).ready(function ($) {
     //------------------------------------------------------------custom
 
 
-    // setTimeout(function(){
-    //     $(".popup-graduation").addClass('is-visible');
-    //     $('.mfp-bg').addClass('is-visible');
-    //     $('html').addClass('lock-html');
-    //
-    //     if(windowWidth > documentWidth){
-    //         $('html').css({
-    //             'margin-right':'17px'
-    //         });
-    //         $('.mfp-wrap').css({
-    //             'overflow-y':'scroll'
-    //         });
-    //     }
-    // }, 3000);
+
 
 
     /*изменение оплаты*/
