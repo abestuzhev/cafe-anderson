6406ab36eaf386b04a0f2b6a9cec476b7ddf1066
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-03-19 11:12:46 +0300

    changes

--- demo/test-catalog.js
+++ demo/test-catalog.js
@@ -1,24 +1,28 @@
 
 $(function(){
-    $('.popup-body').on('click', function(e){
-        e = e || window.event;
-        e.stopPropagation();
-        console.log('click popup');
+    // $('.popup-body').on('click', function(e){
+    //     e = e || window.event;
+    //     e.stopPropagation();
+    //     console.log('click popup');
+    // });
+    // $('document').on('click', function(e){
+    //     $('.mfp-wrap').removeClass('is-visible');
+    //     $('html').find('.mfp-bg ').removeClass('is-visible');
+    //     $('html').removeClass('lock-html').css('margin-right','0');
+    //     console.log('click document');
+    // });
+    //
+    // $('.popup-basket__scroll').on( 'mousewheel DOMMouseScroll', function (e) {
+    //
+    //     var e0 = e.originalEvent;
+    //     var delta = e0.wheelDelta || -e0.detail;
+    //
+    //     this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
+    //     e.preventDefault();
+    // });
+
+    $('.c-card-catalog__img img').lazyload({
+        effect: "fadeIn"
     });
-    $('document').on('click', function(e){
-        $('.mfp-wrap').removeClass('is-visible');
-        $('html').find('.mfp-bg ').removeClass('is-visible');
-        $('html').removeClass('lock-html').css('margin-right','0');
-        console.log('click document');
-    });
-
-    $('.popup-basket__scroll').on( 'mousewheel DOMMouseScroll', function (e) {
-
-        var e0 = e.originalEvent;
-        var delta = e0.wheelDelta || -e0.detail;
-
-        this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
-        e.preventDefault();
-    });
-
+    console.log('lazyload load');
 });
