93c89af025d334eed6ea8c42cc72bdcd0591123e
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-03-20 17:26:11 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@ $(document).ready(function ($) {
             $('#title-party-order').val(letter);
         });
     }
-    changePartyTitlePopup();
+    // changePartyTitlePopup();
 
 
 
@@ -526,6 +526,7 @@ $(document).ready(function ($) {
             if (clickCount == 1) {
                 $(this).attr("data-count", clickCount);
                 $(this).parent('.c-reviews__item').addClass('active');
+                console.log('click');
                 return false;
             } else if (clickCount == 2){
                 showPopup(".c-reviews-positive", '.popup__review');
