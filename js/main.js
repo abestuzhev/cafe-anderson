6843009846c7eec57b11407abd3239067de33b4f
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-05-03 12:12:20 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ $(document).ready(function ($) {
             initCustomSelect('' + list + ' select');
             initCustomSelect('#js-dish-variation__list .panel-dish-variation__item:last-child select');
             initCustomSelect('#js-event-data__list .panel-event-date__item:last-child select');
-            console.log('initCustomSelectSearch');
+            // console.log('initCustomSelectSearch');
             $('.js-input--tel').mask('+7(000)000-00-00', {clearIfNotMatch: true});
             $('.js-input--date').mask('00.00.0000', {clearIfNotMatch: true});
             $('.js-input--mode').mask('с 00:00 до 00:00', {
