3a2d58d4ea54022243cbd82c2723bbbf02c30169
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-05-10 11:56:44 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1311,8 +1311,8 @@ $(document).ready(function ($) {
     // showPopup(".c-reviews-negative", '.popup__review');
     showPopup(".footer-reviews__icon", '.popup__review');
     // showPopup(".popup-forgot-password", '.popup__recovery-password');
-    showPopup(".catalog-product .c-card-catalog__img", '.popup-product');
-    showPopup(".catalog-product .c-card-catalog__title", '.popup-product');
+    showPopup(".js-show-product-order", '.popup-product');
+    // showPopup(".c-card-catalog .c-card-catalog__title", '.popup-product');
     showPopup("#loadCake", '.popup-cake-order');
     showPopup(".cake-card__hover", '.popup-cake-order');
     showPopup(".js-easter-btn", '.popup-cake-order');
@@ -1323,7 +1323,7 @@ $(document).ready(function ($) {
     showPopup(".c-card-event__link--one-click", '.popup-event-one-click');
     showPopup(".excursion-btn", '.popup-excursion-order');
 
-    showPopup(".catalog-product--cake .js-show-cake-order", '.popup-cake-order');
+    showPopup(".js-show-cake-order", '.popup-cake-order.v2');
 
 
 
