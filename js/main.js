5c0f7026d639ee6befc7f32240b72f532d927db2
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-07-30 10:13:05 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -32,7 +32,37 @@ var documentHeight = (document.documentElement.clientHeight );
 // console.log('высота ' + documentHeight);
 
 
-/*анимация покачиания рамки в поводах для праздника holiday-reason-card*/
+window.addEventListener("DOMContentLoaded", function() {
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+        }
+    }
+
+    function mask(event) {
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");
+        if (def.length >= val.length) val = def;
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+        if (event.type == "blur") {
+            if (this.value.length == 2) this.value = ""
+        } else setCursorPosition(this.value.length, this)
+    };
+    var input = document.querySelector(".js-tel");
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+});
 
 
 
