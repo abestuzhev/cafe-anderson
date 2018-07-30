c455a14aabe4a90f7f02ed3a25287f56b5856a70
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-07-30 10:37:25 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -31,39 +31,38 @@ var documentWidth = (document.documentElement.clientWidth ); // ширина м�
 var documentHeight = (document.documentElement.clientHeight );
 // console.log('высота ' + documentHeight);
 
-
-window.addEventListener("DOMContentLoaded", function() {
-    function setCursorPosition(pos, elem) {
-        elem.focus();
-        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-        else if (elem.createTextRange) {
-            var range = elem.createTextRange();
-            range.collapse(true);
-            range.moveEnd("character", pos);
-            range.moveStart("character", pos);
-            range.select()
-        }
+function setCursorPosition(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
     }
+}
 
-    function mask(event) {
-        var matrix = "+7 (___) ___ ____",
-            i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, "");
-        if (def.length >= val.length) val = def;
-        this.value = matrix.replace(/./g, function(a) {
-            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-        });
-        if (event.type == "blur") {
-            if (this.value.length == 2) this.value = ""
-        } else setCursorPosition(this.value.length, this)
-    };
-    var input = document.querySelector(".js-tel");
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-});
+function mask(event) {
+    var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+    if (def.length >= val.length) val = def;
+    this.value = matrix.replace(/./g, function(a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+    if (event.type == "blur") {
+        if (this.value.length == 2) this.value = ""
+    } else setCursorPosition(this.value.length, this)
+};
+var input = document.querySelector(".js-tel");
 
+console.log('hello');
+
+input.addEventListener("input", mask, false);
+input.addEventListener("focus", mask, false);
+input.addEventListener("blur", mask, false);
 
 
 $(window).on('load', function(){
@@ -2769,11 +2768,14 @@ $(function() {
         fit:'cover'
     });
 
-    /* Calendar Selector */
-    new SimpleBar($('.calendar-selector__list')[0], {
-        autoHide: false,
-        scrollbarMinSize: 35,
-    });
+    if($('div').hasClass('calendar-selector__list')){
+        /* Calendar Selector */
+        new SimpleBar($('.calendar-selector__list')[0], {
+            autoHide: false,
+            scrollbarMinSize: 35,
+        });
+    };
+
 
     $('.calendar-title__wrapper').on('click', function (event) {
         event.preventDefault();
