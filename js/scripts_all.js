774e3752b7f723866d0da8423b967f5eae6a9814
TreonID <treonid@gmail.com>
2018-07-30 17:19:30 +0300

    HSL adapt
    without calendar

--- js/scripts_all.js
+++ js/scripts_all.js
@@ -11920,21 +11920,53 @@ $(function() {
     var $target = $('.show-more-target');
     var $control = $('.show-more');
     var target_item_selector = 'div.c-col';
-    var start_count = 8;
+    var hsl_start_count = 8;
+
+    var hsl_sizer = {
+        0: {
+            start_count: 3,
+        },
+        420: {
+            start_count: 3,
+        },
+        620: {
+            start_count: 4,
+        },
+        940: {
+            start_count: 6,
+        },
+        1225: {
+            start_count: 8,
+        },
+    };
     /* end settings */
 
-    var show_more_state_collapsed = true; 
+    var show_more_state_collapsed = true;
     var target_count = $target.find(target_item_selector).length;
 
+    $(window).on('resize', function() {
+        for (var propName in hsl_sizer) {
+            if($(window).width() > propName) {
+                hsl_start_count = hsl_sizer[propName].start_count;
+            }
+        }
+        if(show_more_state_collapsed) {
+            if (hsl_start_count < target_count) {
+                $target.find(target_item_selector).each(function (index, elem) {
+                    index >= hsl_start_count ? $(elem).hide() : $(elem).show();
+                });
+            }
+        }
+
+    });
 
     $control.find('a').on('click', function(event) {
         event.preventDefault();
         show_more_state_collapsed = !show_more_state_collapsed;
-        console.log(show_more_state_collapsed);
 
         $target.find(target_item_selector).each(function(index, elem) {
             if(show_more_state_collapsed) {
-                index >= start_count ? $(elem).hide(): $(elem).show();
+                index >= hsl_start_count ? $(elem).hide(): $(elem).show();
             } else {
                 $(elem).show();
             }
@@ -11950,9 +11982,11 @@ $(function() {
     //        if(current > target_count) $control.hide();
     });
 
-    if(start_count < target_count) {
+
+
+    if(hsl_start_count < target_count) {
         $target.find(target_item_selector).each(function(index, elem) {
-            index >= start_count ? $(elem).hide(): $(elem).show();
+            index >= hsl_start_count ? $(elem).hide(): $(elem).show();
         });
     }
     /* end show more */
